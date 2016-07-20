254f40d3906578b15adbd33684a2456af3d5b420
tkhuynh <thequest2311@yahoo.com>
2016-07-20 02:09:12 -0700

    get all comments and post new comment to mongo

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -75,7 +75,7 @@ var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment) {
       return (
-        <Comment author={comment.author} key={comment.id}>
+        <Comment author={comment.author} key={comment._id}>
           {comment.text}
         </Comment>
       );
--- server.js
+++ server.js
@@ -30,52 +30,49 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API server in conjunction with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+  // Disable caching so we'll always get the latest comments.
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
 app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+  Comment.find(function(err, allComments) {
     if (err) {
       console.error(err);
       process.exit(1);
+    } else {
+      res.json(allComments);
     }
-    res.json(JSON.parse(data));
   });
 });
 
 app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
+  var newComment = new Comment(req.body);
+
+  newComment.save(function(err, savedComment) {
     if (err) {
       console.error(err);
       process.exit(1);
+    } else {
+      Comment.find(function(err, allComments) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        } else {
+          res.json(allComments);
+        }
+      });
     }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
   });
 });
 
